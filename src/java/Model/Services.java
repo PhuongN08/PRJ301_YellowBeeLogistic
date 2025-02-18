796af63fa3a3d4f04b331ac3860976ebc8074a36
PhuongN08 <nguyenluongthephuong14@gmail.com>
2025-02-18 11:12:55 +0700

    newest update

---
+++ src/java/Model/Services.java
@@ -0,0 +1,14 @@
+package Model;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Services {
+
+    private int serviceID;
+    private String serviceName;
+    private String serviceDescribe;
+    private String serviceImage;
+}
